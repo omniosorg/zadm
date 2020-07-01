5806b4839103d469c73f71198e7ae0adbd844bb5
Andy Fiddaman <omnios@citrus-it.co.uk>
2020-07-01 11:12:23 +0000

    OmniOSus.pm - add footer text

--- lib/Zadm/Image/OmniOSus.pm
+++ lib/Zadm/Image/OmniOSus.pm
@@ -7,3 +7,32 @@ has index    => sub { shift->SUPER::baseurl . '/img-us-west.json' };
 has provider => 'omnios-us';
 
 1;
+
+__END__
+
+=head1 COPYRIGHT
+
+Copyright 2020 OmniOS Community Edition (OmniOSce) Association.
+
+=head1 LICENSE
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+You should have received a copy of the GNU General Public License along with
+this program. If not, see L<http://www.gnu.org/licenses/>.
+
+=head1 AUTHOR
+
+S<Andy Fiddaman E<lt>andy@omnios.orgE<gt>>
+
+=head1 HISTORY
+
+2020-07-01 af Initial Version
+
+=cut
