d541244a37322e3209308981d8a0c04ab6bb1638
Dominik Hassler <hadfl@omniosce.org>
2020-07-07 15:19:56 +0000

    remove quotes from curl write-out format

--- lib/Zadm/Utils.pm
+++ lib/Zadm/Utils.pm
@@ -8,7 +8,6 @@ use Mojo::Log;
 use Mojo::File;
 use Mojo::JSON qw(decode_json);
 use Mojo::Message::Response;
-use Mojo::Util qw(unquote);
 use IPC::Open3;
 use File::Spec;
 use File::Temp;
@@ -153,13 +152,12 @@ sub curl {
     my $url  = shift;
     my $opts = shift // [];
 
-    my @cmd = ($CMDS{curl}, @$opts, qw(-L -w "%{json}" -o), $file, $url);
+    my @cmd = ($CMDS{curl}, @$opts, qw(-L -w %{json} -o), $file, $url);
 
     open my $curl, '-|', @cmd
         or Mojo::Exception->throw("ERROR: executing 'curl': $!\n");
 
-    # curl returns the json object quoted
-    my $status = decode_json(unquote(<$curl>)) // {};
+    my $status = decode_json(<$curl>) // {};
 
     my $res = Mojo::Message::Response->new;
     $res->code($status->{http_code});
