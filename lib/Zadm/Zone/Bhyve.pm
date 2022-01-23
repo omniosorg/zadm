06760407db8f558727dae393ec4604668edba5a0
Dominik Hassler <hadfl@omnios.org>
2022-01-23 16:19:08 +0000

    add option to attach to console for shutdown/reboot

--- lib/Zadm/Zone/Bhyve.pm
+++ lib/Zadm/Zone/Bhyve.pm
@@ -196,8 +196,8 @@ where 'command' is one of the following:
     vacuum [-d <days>]
     brands
     start [-c [extra_args]] <zone_name>
-    stop <zone_name>
-    restart <zone_name>
+    stop [-c [extra_args]] <zone_name>
+    restart [-c [extra_args]] <zone_name>
     poweroff <zone_name>
     reset <zone_name>
     nmi <zone_name>
@@ -215,7 +215,7 @@ where 'command' is one of the following:
 
 =head1 COPYRIGHT
 
-Copyright 2021 OmniOS Community Edition (OmniOSce) Association.
+Copyright 2022 OmniOS Community Edition (OmniOSce) Association.
 
 =head1 LICENSE
 
